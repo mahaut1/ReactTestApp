dfb20f78a355cd65403d7da0394e9451e70a868f
Mahaut Windal <mwindal@hotmail.com>
2025-04-15 11:44:25 +0200

    BDD Docker

---
+++ react_test_app/SQLFiles/migration-v002.sql
@@ -0,0 +1,8 @@
+USE ynov_ci;
+CREATE table IF NOT EXISTS ynov_ci.users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
