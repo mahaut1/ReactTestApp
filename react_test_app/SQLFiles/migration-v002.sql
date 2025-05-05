06079aea9c888f699446e3ecb8b38c19407190f6
Mahaut Windal <mwindal@hotmail.com>
2025-05-05 12:33:32 +0200

    docker compose Dockerfile

--- react_test_app/SQLFiles/migration-v002.sql
+++ react_test_app/SQLFiles/migration-v002.sql
@@ -6,3 +6,10 @@ CREATE table IF NOT EXISTS ynov_ci.users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO ynov_ci.users (name, email, password) VALUES
+('Alice Dupont', 'alice@example.com', 'motdepasse1'),
+('Bob Martin', 'bob@example.com', 'motdepasse2'),
+('Chloé Bernard', 'chloe@example.com', 'motdepasse3'),
+('David Lefèvre', 'david@example.com', 'motdepasse4'),
+('Emma Moreau', 'emma@example.com', 'motdepasse5');
