ee59ee83661e9851361b5c167f14ab4338edfbde
Mahaut Windal <mwindal@hotmail.com>
2025-05-26 16:22:13 +0200

    essai 10

--- react_test_app/cypress/cypress.config.js
+++ react_test_app/cypress/cypress.config.js
@@ -2,9 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // Indique le dossier où se trouvent tes tests end-to-end
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-
-    baseUrl: 'http://localhost:3000', // adapte selon ton app React
+    baseUrl: 'http://localhost:3000',
   },
 })
