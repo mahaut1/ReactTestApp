f3f7980f498ccd08457afc67e9c753aaeb808b5d
Mahaut Windal <mwindal@hotmail.com>
2025-05-26 22:07:05 +0200

    formulaire d'enregistrement

--- react_test_app/src/App.js
+++ react_test_app/src/App.js
@@ -5,7 +5,7 @@ import UserList from "./UserList";
 function App() {
   return (
     <div>
-      <h1>Gestion des utilisateurs</h1>
+      <h1>Formulaire d'enregistrement</h1>
       <RegistrationForm />
       <UserList />
     </div>
