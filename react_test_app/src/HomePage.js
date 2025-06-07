4ae58a626c225dd82863d8ef90c61764bb6d79b1
Mahaut Windal <mwindal@hotmail.com>
2025-06-07 17:52:40 +0200

    HomePage

---
+++ react_test_app/src/HomePage.js
@@ -0,0 +1,11 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Bienvenue</h2>
+      <p>Utilisez les liens pour vous inscrire, vous connecter ou accéder à la page admin si vous êtes autorisé.</p>
+    </div>
+  );
+};
+
+export default HomePage;
