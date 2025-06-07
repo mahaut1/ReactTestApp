f6c9f62c96ac5bf4d51b94578cd7559f20368dab
Mahaut Windal <mwindal@hotmail.com>
2025-06-07 23:05:00 +0200

    test 3

--- react_test_app/src/App.js
+++ react_test_app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
 import LoginPage from './LoginFile';
 import AdminPage from './AdminPage';
@@ -9,27 +9,25 @@ import HomePage from './HomePage';
 
 function App() {
   return (
-    <Router basename="/ReactTestApp">
-      <div className="App">
-        <header className="App-header">
-          <h1>User Manager</h1>
-        </header>
+    <div className="App">
+      <header className="App-header">
+        <h1>User Manager</h1>
+      </header>
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/admin"
-            element={
-              <RequireAdmin>
-                <AdminPage />
-              </RequireAdmin>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/admin"
+          element={
+            <RequireAdmin>
+              <AdminPage />
+            </RequireAdmin>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
--- react_test_app/src/index.test.js
+++ react_test_app/src/index.test.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import * as reportWebVitalsModule from './reportWebVitals'; // Importer le module entier
 
 test('renders App component', () => {
-  render(<App />);
-  expect(screen.getByText(/Formulaire d'Enregistrement/i)).toBeInTheDocument();
-});
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
 
+  expect(screen.getByText(/Bienvenue/i)).toBeInTheDocument();
+});
