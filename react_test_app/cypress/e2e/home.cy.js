fb3ec3fe48b4238ae453ca16bf5cc4a55e4cf2b8
Mahaut Windal <mwindal@hotmail.com>
2025-06-08 00:04:36 +0200

    homepage essai 2

--- react_test_app/cypress/e2e/home.cy.js
+++ react_test_app/cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('Homepage', () => {
     it('Loads homepage and shows form', () => {
-      cy.visit('http://localhost:3000');
+      cy.visit('http://localhost:3000/ReactTestApp');
       cy.contains('Bienvenue');
     });
   });
