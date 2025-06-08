e5a6c02c149d9e6c2a16f3b3d41093dda8a00c94
Mahaut Windal <mwindal@hotmail.com>
2025-06-08 13:43:37 +0200

    lien redirection login register page accueil

--- react_test_app/src/HomePage.js
+++ react_test_app/src/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
       <h2>Bienvenue</h2>
-      <p>Utilisez les liens pour vous inscrire, vous connecter ou accéder à la page admin si vous êtes autorisé.</p>
+      <p>Utilisez les liens ci-dessous pour vous inscrire, vous connecter ou accéder à la page admin si vous êtes autorisé.</p>
+      <ul>
+        <li><Link to="/register">Inscription</Link></li>
+        <li><Link to="/login">Connexion</Link></li>
+        <li><Link to="/admin">Page Admin</Link></li>
+      </ul>
     </div>
   );
 };
