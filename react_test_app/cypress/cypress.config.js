f79fb83c8b63d2c338f1560e388547a540e1a306
Mahaut Windal <mwindal@hotmail.com>
2025-06-13 23:45:40 +0200

    E2E essai 2

--- react_test_app/cypress/cypress.config.js
+++ react_test_app/cypress/cypress.config.js
@@ -4,5 +4,9 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:3000',
+    env: {
+      ADMIN_EMAIL: process.env.CYPRESS_ADMIN_EMAIL,
+      ADMIN_PASSWORD: process.env.CYPRESS_ADMIN_PASSWORD,
+    },
   },
 })
